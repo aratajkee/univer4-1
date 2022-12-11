e245f4c5340135d74fa9f60e3079a058590b76ac
Denis <denniskost1n@yandex.ru>
2022-12-11 20:29:37 +0300

    userListActivity created thread to read data from SQLtable and fill ListView

--- app/src/main/java/com/example/laba2authform/UsersListActivity.java
+++ app/src/main/java/com/example/laba2authform/UsersListActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -22,15 +25,91 @@ public class UsersListActivity extends AppCompatActivity {
     DatabaseHelper databaseHelper;
     private ListView listView;
     private static final String TAG = "UserListActivity";
+    Looper looper;
+    Cursor data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_users_list);
         Log.d(TAG, " Started this Activity");
+
         listView = findViewById(R.id.userlist);
+
         databaseHelper = new DatabaseHelper(this);
-        populateListView();
+        looper = Looper.getMainLooper();
+        final Handler handler = new Handler(looper) {
+            public void handleMessage(Message message) {
+
+                data = (Cursor) message.obj;
+                populateListView();
+            }
+
+        };
+
+        new Thread(() -> {
+
+            Log.d(TAG, "Started thread to get data from database");
+            final Message message = Message.obtain();
+            message.obj = databaseHelper.getData();
+            handler.sendMessage(message);
+
+        }).start();
+
+
+    }
+
+    private void populateListView() {
+
+        //Cursor data = databaseHelper.getData();
+        ArrayList<String> listData = new ArrayList<>();
+
+        while (data.moveToNext()) {
+            listData.add(data.getString(1));
+        }
+        ListAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listData);
+        listView.setAdapter(adapter);
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String item = adapterView.getItemAtPosition(i).toString();
+                String pass = "ERROR";
+                Log.d(TAG, " onItemClick: You Clicked On " + item);
+
+                Cursor data = databaseHelper.getItemId(item);
+                int itemId = -1;
+                while (data.moveToNext()) {
+                    itemId = data.getInt(0);
+                }
+                if (itemId > -1) {
+                    Log.d(TAG, " onItemClick: The ID Is: " + itemId);
+                    Intent editScreenIntent = new Intent(UsersListActivity.this, EditDataActivity.class);
+                    editScreenIntent.putExtra("id", itemId);
+                    editScreenIntent.putExtra("item", item);
+
+                    Cursor passdata = databaseHelper.getPass(String.valueOf(itemId), item);
+                    while (passdata.moveToNext()) {
+                        pass = passdata.getString(0);
+                    }
+                    editScreenIntent.putExtra("pass", pass);
+                    startActivity(editScreenIntent);
+                } else {
+                    toastMessage("No ID For This Name!");
+                }
+            }
+        });
+    }
+
+
+    public void goHome(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    private void toastMessage(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -44,6 +123,7 @@ public class UsersListActivity extends AppCompatActivity {
         super.onPause();
         Log.d(TAG, " Paused this Activity");
     }
+
     @Override
     protected void onResume() {
         super.onResume();
@@ -53,8 +133,9 @@ public class UsersListActivity extends AppCompatActivity {
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d(TAG," STOPED THIS ACTIVITY!");
+        Log.d(TAG, " STOPED THIS ACTIVITY!");
     }
+
     @Override
     protected void onRestart() {
         super.onRestart();
@@ -64,57 +145,6 @@ public class UsersListActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.d(TAG," DESTROYED THIS ACTIVITY!");
-    }
-
-    private void populateListView() {
-
-        Cursor data = databaseHelper.getData();
-        ArrayList<String> listData = new ArrayList<>();
-        while (data.moveToNext()) {
-            listData.add(data.getString(1));
-        }
-        ListAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listData);
-        listView.setAdapter(adapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String item = adapterView.getItemAtPosition(i).toString();
-                String pass = "ERROR";
-                Log.d(TAG," onItemClick: You Clicked On "+ item);
-
-                Cursor data = databaseHelper.getItemId(item);
-                int itemId = -1;
-                while (data.moveToNext()){
-                    itemId = data.getInt(0);
-                }
-                if(itemId>-1){
-                    Log.d(TAG, " onItemClick: The ID Is: "+itemId);
-                    Intent editScreenIntent = new Intent(UsersListActivity.this, EditDataActivity.class);
-                    editScreenIntent.putExtra("id",itemId);
-                    editScreenIntent.putExtra("item",item);
-
-                    Cursor passdata = databaseHelper.getPass(String.valueOf(itemId),item);
-                    while (passdata.moveToNext()){
-                        pass = passdata.getString(0);
-                    }
-                    editScreenIntent.putExtra("pass",pass);
-                    startActivity(editScreenIntent);
-                }else{
-                    toastMessage("No ID For This Name!");
-                }
-            }
-        });
-    }
-
-
-    public void goHome(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-    }
-
-    private void toastMessage(String message){
-        Toast.makeText(this,message, Toast.LENGTH_SHORT).show();
+        Log.d(TAG, " DESTROYED THIS ACTIVITY!");
     }
 }
