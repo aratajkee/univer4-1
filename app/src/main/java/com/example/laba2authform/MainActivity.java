40bca671949b7e3655a37a1a39c4ff4d7cfa54ce
Denis <denniskost1n@yandex.ru>
2022-12-11 19:58:04 +0300

    add user thread done

--- app/src/main/java/com/example/laba2authform/MainActivity.java
+++ app/src/main/java/com/example/laba2authform/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -12,9 +15,9 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 
-public class MainActivity extends AppCompatActivity  {
+public class MainActivity extends AppCompatActivity {
 
-    private  final String TAG = "MainActivity";
+    private final String TAG = "MainActivity";
     DatabaseHelper databaseHelper;
     UserList userArray = new UserList();
 
@@ -23,29 +26,63 @@ public class MainActivity extends AppCompatActivity  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG, " Started This Activity!");
-        Button buttonAdd =(Button) findViewById(R.id.button_add_user);
+
+        Button buttonAdd = (Button) findViewById(R.id.button_add_user);
         Button triger_btn = (Button) findViewById(R.id.triger_btn);
         EditText userName = (EditText) findViewById(R.id.user_name);
         EditText passWord = (EditText) findViewById(R.id.user_pass);
         Button logout_btn = (Button) findViewById(R.id.logout_btn);
+
         databaseHelper = new DatabaseHelper(MainActivity.this);
+
+        final Looper looper = Looper.getMainLooper();
+        final Handler handler = new Handler(looper) {
+            public void handleMessage(Message message) {
+                if ((boolean) message.obj) {
+                    toastMessage("New user added!");
+                } else {
+                    toastMessage("Failed to add new user!");
+                }
+            }
+        };
 
         buttonAdd.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                User user = new User();
-                user.setUsername(userName.getText().toString());
-                user.setPassword(passWord.getText().toString());
-                userArray.addUser(user.getUsername());
+                String username = userName.getText().toString().trim();
+                String password = passWord.getText().toString().trim();
 
-                if(userName.length() != 0){
-                   databaseHelper.addData(userName.getText().toString().trim(), passWord.getText().toString().trim());
-                    userName.setText("");
-                    passWord.setText("");
-                }else{
-                    toastMessage("Введите данные пользователя!");
-                }
 
+                new Thread(() -> {
+                    Log.d(TAG, " started adding user thread");
+                    if (username.length() != 0 && password.length() != 0) {
+                        Looper.prepare();
+                        databaseHelper.addData(username, password);
+
+                        userName.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                userName.setText("");
+                            }
+                        });
+
+                        passWord.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                passWord.setText("");
+                            }
+                        });
+
+                        final Message message = Message.obtain();
+                        message.obj = true;
+                        handler.sendMessage(message);
+                    } else {
+                        final Message message = Message.obtain();
+                        message.obj = false;
+                        handler.sendMessage(message);
+                    }
+
+                }).start();
 
             }
         });
@@ -53,7 +90,7 @@ public class MainActivity extends AppCompatActivity  {
         triger_btn.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),TrigerPause.class);
+                Intent intent = new Intent(getApplicationContext(), TrigerPause.class);
                 startActivity(intent);
             }
         });
@@ -65,7 +102,8 @@ public class MainActivity extends AppCompatActivity  {
             }
         });
     }
-    private void logout(){
+
+    private void logout() {
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
@@ -91,7 +129,7 @@ public class MainActivity extends AppCompatActivity  {
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d(TAG," STOPED THIS ACTIVITY!");
+        Log.d(TAG, " STOPED THIS ACTIVITY!");
     }
 
     @Override
@@ -103,18 +141,18 @@ public class MainActivity extends AppCompatActivity  {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.d(TAG," DESTROYED THIS ACTIVITY!");
+        Log.d(TAG, " DESTROYED THIS ACTIVITY!");
     }
 
-    public void goUserList(View view){
+    public void goUserList(View view) {
         Intent intent = new Intent(this, UsersListActivity.class);
         startActivity(intent);
     }
-    private void toastMessage(String message){
+
+    private void toastMessage(String message) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
 
     }
-
 
 
 }
