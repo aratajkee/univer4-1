c7def730294869b1b31156f2185385ffb31dd8db
Denis <denniskost1n@yandex.ru>
2022-12-11 19:35:03 +0300

    login thread done

--- app/src/main/java/com/example/laba2authform/LoginActivity.java
+++ app/src/main/java/com/example/laba2authform/LoginActivity.java
@@ -2,6 +2,9 @@ package com.example.laba2authform;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.os.PersistableBundle;
 import android.util.Log;
 import android.view.View;
@@ -23,11 +26,27 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
         Log.d(TAG, " Started This Activity!");
+
         EditText username_field = (EditText) findViewById(R.id.username_login);
         EditText password_field = (EditText) findViewById(R.id.password_login);
         Button login_btn = (Button) findViewById(R.id.login_btn);
         Button getmein_btn = (Button) findViewById(R.id.getmein_btn);
+
+        final Looper looper = Looper.getMainLooper();
         databaseHelper = new DatabaseHelper(LoginActivity.this);
+
+        final Handler handler = new Handler(looper){
+            public void handleMessage(Message message){
+
+                if ((boolean) message.obj){
+                    getMeIn();
+                }else {
+                    username_field.setText("");
+                    password_field.setText("");
+                    toastMessage("Enter correct login");
+                }
+            }
+        };
 
         getmein_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -71,18 +90,17 @@ public class LoginActivity extends AppCompatActivity {
                     Log.d(TAG, " statrted login thread");
                     if (databaseHelper.login(username, password)){
 
-                        Log.d(TAG, "SUCSESSSSSSSS");
-                        //
+                        final Message message = Message.obtain();
+                        message.obj = true;
+                        handler.sendMessage(message);
+
                     }else {
-                        Log.d(TAG, "PIZDEC");
+                        final Message message = Message.obtain();
+                        message.obj = false;
+                        handler.sendMessage(message);
+
                     }
                 }).start();
-
-
-
-
-
-
 
             }
         });
